21d50597ddee0babaee8b270f3ed3ddc63e88b2a
Milos <milosgolub97@gmail.com>
2020-12-03 01:49:36 +0000

    Starting up, creating file #1

---
+++ 1 - Starting with bubble sort/main.cpp
@@ -0,0 +1,36 @@
+#include <vector>
+#include <iostream>
+
+void bubbleSort(std::vector<int>& a) {
+    for(unsigned i = 0; i < a.size(); ++i) {
+        for(unsigned j = 1; j < a.size() - i; ++j) {
+            if(a[j] < a[j-1]) {
+                int tmp = a[j];
+                a[j] = a[j-1];
+                a[j-1] = tmp;
+            }
+        }
+    }
+}
+
+void printVector(const std::vector<int>& a) {
+    for(unsigned i = 0; i < a.size(); ++i) {
+        std::cout << a[i] << ' ';
+    }
+    std::cout << std::endl;
+}
+
+void printSortPrint(std::vector<int>& a) {
+    std::cout << "Before: ";
+    printVector(a);
+    bubbleSort(a);
+    std::cout << "After: ";
+    printVector(a);
+}
+
+
+int main() {
+    std::vector<int> vec{ 37, 9, 100, 13 };
+    printSortPrint(vec);
+    return 0;
+}
