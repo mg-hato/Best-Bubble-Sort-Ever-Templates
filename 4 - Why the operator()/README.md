39749160775ab25a65d3c274be0bcf572f0f2a3a
Milos <milosgolub97@gmail.com>
2021-01-11 19:03:07 +0000

    Finalisation

--- 4 - Why the operator()/README.md
+++ 4 - Why the operator()/README.md
@@ -1,1 +1,10 @@
-# TODO
+# Why the operator()?
+
+No particular reason. It is chosen because of the consistency with other comparators of similiar nature.
+
+For example, both priority queue and (ordered) set use such comparators.
+And comparators providing `<`-operator for type `T` are identified as `std::less<T>`, which also implement `operator()`.
+
+As such, on line 39 we use `decimalDigitOrdering` discussed previously and pass it as the comparator for native C++ priority queue.
+
+As such, the simple journey of C++ templates ends here. 
--- 4 - Why the operator()/main.cpp
+++ 4 - Why the operator()/main.cpp
@@ -2,21 +2,50 @@
 #include <queue>
 #include <iostream>
 #include <vector>
+#include <set>
 
-class oddLessThanEven {
+// class that defines decimal digit ordering
+class decimalDigitsOrdering {
 public:
     bool operator()(const int x, const int y) {
-        return x % 2 != 0 && y % 2 == 0;
+        return digitCount(x) < digitCount(y);
+    }
+
+private:
+    unsigned digitCount(int n) {
+        if(n == 0) return 1;
+
+        unsigned digCount = 0;
+        if(n < 0) n = -n;
+        while(n > 0) {
+            n /= 10; ++digCount;
+        }
+        return digCount;
     }
 };
 
-void screaming() {
-    return "Screaming";
+template <class T>
+void printVector(const std::vector<T>& a) {
+    for(unsigned i = 0; i < a.size(); ++i) {
+        std::cout << a[i] << ' ';
+    }
+    std::cout << std::endl;
 }
 
 int main() {
-    std::vector<int> x{ 11, 21, 35, 43 };
-    std::priority_queue<int, std::vector<int>, screaming> pq(x.begin(), x.end());
+    std::vector<int> x{ 21, 11, 10000, 35, 43, 111 };
+
+    // priority queue whose top element is some number written with most digits (in decimal)
+    std::priority_queue<int, std::vector<int>, decimalDigitsOrdering> pq(x.begin(), x.end());
     std::cout << pq.top() << std::endl;
+
+    // ordered set usng `<` operator to order
+    std::set<int, std::less<int>> numbers(x.begin(), x.end());
+
+    auto lb = numbers.lower_bound(21);
+    auto ub = numbers.upper_bound(21);
+    std::cout << "Lower & upper bound of 21: " << *lb << ' ' << *ub << std::endl;
+
+    printVector(x);
     return 0;
 }
